991fdd95cdefe820117bb28f09b2077ea6e181a3
Sergiy Beley <sergiy.beley@gmail.com>
2014-11-29 20:55:10 +0200

    Country repository and endpoint

---
+++ src/main/java/com/sbelei/minihotel/endpoint/CountryEndpoint.java
@@ -0,0 +1,30 @@
+package com.sbelei.minihotel.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.sbelei.minihotel.owner.GetCountryRequest;
+import com.sbelei.minihotel.owner.GetCountryResponse;
+import com.sbelei.minihotel.repository.CountryRepository;
+
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://sbelei.com/minihotel/owner";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
