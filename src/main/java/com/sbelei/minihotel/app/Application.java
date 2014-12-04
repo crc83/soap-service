19ad4ba4b9e9bc89d3ab5f1296f2b87f1f77412f
Sergiy Beley <Sergiy.Beley@gmail.com>
2014-12-04 02:17:53 +0200

    Add componentscan

--- src/main/java/com/sbelei/minihotel/app/Application.java
+++ src/main/java/com/sbelei/minihotel/app/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
+@ComponentScan(basePackages={"com.sbelei.minihotel.endpoint", "com.sbelei.minihotel.repository", "com.sbelei.minihotel.wsconfig"})
 @EnableAutoConfiguration
 public class Application {
 
