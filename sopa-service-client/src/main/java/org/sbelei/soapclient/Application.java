c7cf10bc928e44d307b76d99974b9509df4844f3
Sergiy Beley <Sergiy.Beley@gmail.com>
2014-12-07 22:05:55 +0200

    soap service invoked from java code

--- sopa-service-client/src/main/java/org/sbelei/soapclient/Application.java
+++ sopa-service-client/src/main/java/org/sbelei/soapclient/Application.java
@@ -1,5 +1,7 @@
 package org.sbelei.soapclient;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.sbelei.wsdl.GetCountryResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 
@@ -9,10 +11,9 @@ public class Application {
 		ApplicationContext ctx = SpringApplication.run(CountryConfiguration.class, args);
 
 		CountryClient countryClient = ctx.getBean(CountryClient.class);
-
+		GetCountryResponse responce = countryClient.getCountryInfo("Ukraine");
 		
-//		GetCityForecastByZIPResponse response = weatherClient.getCityForecastByZip(zipCode);
-//		weatherClient.printResponse(response);
+		System.out.println( ToStringBuilder.reflectionToString(responce.getCountry()) );
 	}
 
 }
--- sopa-service-client/src/main/java/org/sbelei/soapclient/CountryClient.java
+++ sopa-service-client/src/main/java/org/sbelei/soapclient/CountryClient.java
@@ -1,7 +1,16 @@
 package org.sbelei.soapclient;
 
+import org.sbelei.wsdl.GetCountryRequest;
+import org.sbelei.wsdl.GetCountryResponse;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class CountryClient extends WebServiceGatewaySupport {
+	
+	public GetCountryResponse getCountryInfo(String countryName){
+		GetCountryRequest request = new GetCountryRequest();
+		request.setName(countryName);
+		
+		return (GetCountryResponse) getWebServiceTemplate().marshalSendAndReceive(request);
+	}
 
 }
